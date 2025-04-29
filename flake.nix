2b9f5a9670bec8f313d8e83a5ee0e98a06a85ffc
xiongchenyu <xiongchenyu6@gmail.com>
2025-04-29 18:11:13 +0800

    init
    Signed-off-by: xiongchenyu <xiongchenyu6@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,52 @@
+# SPDX-FileCopyrightText: 2021 Serokell <https://serokell.io/>
+#
+# SPDX-License-Identifier: CC0-1.0
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    { nixpkgs, flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          lib,
+          ...
+        }:
+        {
+          
+          devShells.default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+            RUST_BACKTRACE = 1;
+            nativeBuildInputs = with pkgs; [
+              nixfmt-rfc-style
+              nixd
+              bun
+              pkg-config
+              rustc
+              cargo
+              rust-analyzer
+              clippy
+              openssl
+              rustfmt          
+              wasm-pack      
+                wasm-bindgen-cli
+                clang
+                lld
+            ];
+          };
+        };
+    };
+}
